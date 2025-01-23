cfa25a7c0f6b428a09c6daee29864d48e807232a
mohamed.karawia <m.karawia11@gmail.com>
2025-01-23 20:08:45 +0200

    [chore] change wallet icons constant type

--- src/constants/WALLET_ICONS.ts
+++ src/constants/WALLET_ICONS.ts
@@ -3,7 +3,7 @@ import CoinBaseIcon from "../../public/icons/coinbase.svg";
 import PhantomIcon from "../../public/icons/phantom.svg";
 import WalletConnectIcon from "../../public/icons/wallet-connect.svg";
 
-const WALLETS_ICONS: any = {
+const WALLETS_ICONS: Record<string, string> = {
   MetaMask: MetaMaskIcon,
   "Coinbase Wallet": CoinBaseIcon,
   Phantom: PhantomIcon,
