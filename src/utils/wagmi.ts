a1b8b181b81f3028171ea1d3695d9838da75c574
mohamed.karawia <m.karawia11@gmail.com>
2025-01-24 16:26:26 +0200

    [chore] add comment to projectid

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -2,13 +2,16 @@ import { http, cookieStorage, createConfig, createStorage } from "wagmi";
 import { mainnet, bsc } from "wagmi/chains";
 import { coinbaseWallet, metaMask, walletConnect } from "wagmi/connectors";
 
+// This must be in env file but i'm leaving it here for simplicity
+const PROJECT_ID = "20642d3cf110a601de466dba9b0d4206";
+
 export const getConfig = () => {
   return createConfig({
     chains: [mainnet, bsc],
     connectors: [
       metaMask(),
       coinbaseWallet(),
-      walletConnect({ projectId: "20642d3cf110a601de466dba9b0d4206" }),
+      walletConnect({ projectId: PROJECT_ID }),
     ],
     transports: {
       [mainnet.id]: http(),
